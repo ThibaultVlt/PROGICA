b837c647fa01adedd53b765acf977f3129a8d6bb
Thibault <thibvollet@gmail.com>
2025-01-03 10:35:25 +0100

    Correction de bug de carrousel en page accueil

--- resources/views/gites/card_gites.blade.php
+++ resources/views/gites/card_gites.blade.php
@@ -17,7 +17,7 @@
               <div class="card">
                 <div>
                     @if($gite->photos)
-                        <div id="carouselPhotos" class="carousel slide" data-bs-ride="carousel">
+                        <div id="carouselPhotos{{ $gite->id }}" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 @foreach(json_decode($gite->photos, true) as $index => $photo)
                                     <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
@@ -25,11 +25,11 @@
                                     </div>
                                 @endforeach
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselPhotos" data-bs-slide="prev">
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselPhotos{{ $gite->id }}" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselPhotos" data-bs-slide="next">
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselPhotos{{ $gite->id }}" data-bs-slide="next">
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
